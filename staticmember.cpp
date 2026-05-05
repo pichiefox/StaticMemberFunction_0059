1dcefa34b24c785b4dbd8d263067824585e734e5
pichiefox <citrazad@gmail.com>
2026-05-05 16:56:54 +0700

    mengimplementasikan prosedur - prosedur di luar class

--- staticmember.cpp
+++ staticmember.cpp
@@ -23,3 +23,15 @@ public:
 //memberi value ke variabel nim dari class mahasiswa
 int Mahasiswa::nim = 10;
 
+//mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa: :setID() {
+    id = ++nim;
+};
+
+void Mahasiswa :: printAll() {
+
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+};
+
