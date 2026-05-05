16db55e1bc07ba2e14fe37e937cac1bb7a220eac
pichiefox <citrazad@gmail.com>
2026-05-05 17:59:02 +0700

    Member Public

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -29,35 +29,3 @@ public:
 
 int Mahasisma :: nim = 0;
 
-//pendefinisial prosedur - prosedur diluar class
-void Mahasisma :: setID() {
-    id = ++nim;
-}
-
-void Mahasiswa: :printAll() {
-    cout << "ID = " << id << endl;
-    cout << "Nama = " << nama << endl;
-    cout << endl;
-}
-
-int main() {
-    //pembuatan object dan pemberian nilai
-    Mahasiswa mhs1("Sri Dadi");
-    Mahasiswa mhs2("Budi Jatmiko");
-    //memberi nilai pada setNim() untuk merubah nilai NIM
-    Mahasiswa :: setNim(9);
-    Mahasiswa mhs3("Andi Janu");
-    Mahasiswa mhs4("Joko Wahono");
-
-    //memanggil prosedur printAll()
-    mhs1.printAll();
-    mhs2.printAll();
-    mhs3.printAll();
-    mhs4.printAll();
-
-    //menampilkan NIM terakhir yang diproses
-    cout << "Akses dari luar object = " << Mahasisma :: getNim() << endl;
-    system("pause");
-
-    return 0;
-}
